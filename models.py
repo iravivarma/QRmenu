ab0e1a31d24e5040feb3827b6d9173623d121062
krishnardt <krishnardt365@gmail.com>
2021-02-27 16:29:14 +0530

    make it better

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mymenu"
+SQLALCHEMY_DATABASE_URL = "postgres://grgztuvnabdzsz:d1204bf882973a09ef3231b59d8975fc195d3f8cadfb07f130caa5ebeb188fef@ec2-54-90-13-87.compute-1.amazonaws.com:5432/db0n84qslnukga"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
