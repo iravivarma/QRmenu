25e20991916f440b040d671124a6f61ca515e027
krishnardt <krishnardt365@gmail.com>
2021-02-27 16:25:51 +0530

    make it better

--- database.py
+++ database.py
@@ -20,7 +20,8 @@ password = 'postgres'
 ip_address = 'localhost'
 port = '5432'
 db = 'mymenu'
-SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{ip_address}:{port}/{db}"
+#SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{ip_address}:{port}/{db}"
+SQLALCHEMY_DATABASE_URL = "postgres://grgztuvnabdzsz:d1204bf882973a09ef3231b59d8975fc195d3f8cadfb07f130caa5ebeb188fef@ec2-54-90-13-87.compute-1.amazonaws.com:5432/db0n84qslnukga"
 log_info(logging, SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
